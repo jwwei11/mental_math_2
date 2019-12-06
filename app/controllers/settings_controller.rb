9e24a9e113ef151284a597855a4922142c9ddff9
Raghu Betina <raghu@firstdraft.com>
2019-12-06 20:05:15 +0000

    Only setting user can access show page

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,5 @@
 class SettingsController < ApplicationController
-  before_action :current_user_must_be_setting_owner, :only => [:edit_form, :update_row, :destroy_row]
+  before_action :current_user_must_be_setting_owner, :only => [:show, :edit_form, :update_row, :destroy_row]
 
   def current_user_must_be_setting_owner
     setting = Setting.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
