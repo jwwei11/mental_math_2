6de5e6aabd6d5cccd733a3fdfd7ee7d186710112
Raghu Betina <raghu@firstdraft.com>
2019-12-06 20:05:15 +0000

    Only setting user can modify setting

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,14 @@
 class SettingsController < ApplicationController
+  before_action :current_user_must_be_setting_owner, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_setting_owner
+    setting = Setting.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == setting.owner
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @settings = Setting.all
 
